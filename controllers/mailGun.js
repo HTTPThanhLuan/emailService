59e0ec0c760858ab62f30bd40d703a5e88a4b99e
ThanhLuan <luan.tt87@gmail.com>
2020-03-29 22:03:54 -0500

    check in
    check in

--- controllers/mailGun.js
+++ controllers/mailGun.js
@@ -1,29 +1,28 @@
+const MailGun = require('../models/mailgun')
 const joi = require('joi');
 require('dotenv').config();
 const mailgun = require('mailgun-js')({ apiKey: process.env.MyMailGunKey, domain: process.env.MyMailGunDomain })
 
 module.exports.send = function(req,res, next){
+     const body =  req.body;
+     //Validate here. 
+     const schema={
+      from:joi.string().required(),
+      to: joi.string().required(),
+      subject: joi.string().min(5).required(),
+  //  text: joi.string().min(5).required(),
+      html:joi.string().required(),          
+     };
+     const result = joi.validate(body,schema);
+     //400 Badrequest
+     if(result.error) return res.status(400).send(result.error.details[0].message);
+     console.log(process.env.MyMailGunKey);
+      mailgun.messages().send(body,(err,data)=>{
       
-    //Validate here. 
-    const schema={
-       from:joi.string().required(),
-       to: joi.string().required(),
-       subject: joi.string().min(5).required(),
-     //  text: joi.string().min(5).required(),
-       html:joi.string().required(),          
-    }
- 
-    const result = joi.validate(req.body,schema);
+         res.send(data);
+     });
 
-    //400 Badrequest
-    if(result.error) return res.status(400).send(result.error.details[0].message);
- 
+
    
-    //Send email
-    mailgun.messages().send(req.body, (error, body) => {     
-        res.send(body);
-      });
-
-    console.log('we are in mailGun controller');
-      
+           
 }
--- models/mailgun.js
+++ models/mailgun.js
@@ -1,12 +1,22 @@
-class mailGun{
-    
-    //TODO: stuck here
-    from: String
 
+require('dotenv').config();
+const mailgun = require('mailgun-js')({ apiKey: process.env.MyMailGunKey, domain: process.env.MyMailGunDomain })
+
+
+module.exports = class mailGun{
     
-    constructor(){
+          
+    constructor(from, to, subject, html ){
+        this.from=from;
+        this.to=to;
+        this.subject=subject;
+        this.html=html;
+    }
+    send(){
+             //Send email
+     return mailgun.messages().send(this);
 
     }
 
 
-}
+};
